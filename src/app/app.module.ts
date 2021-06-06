09891bba7315d7a5a31947665b7556d16a15f69a
ricktes <occa2002@gmail.com>
2021-06-05 22:55:15 -0500

    начал работать с проектом Heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TextComponent } from './components/text/text.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 
 @NgModule({
@@ -14,11 +18,20 @@ import { HomeComponent } from './components/home/home.component';
     NavbarComponent,
     TextComponent,
     HomeComponent,
+    HeroesComponent,
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+
+// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+// and returns simulated server responses.
+// Remove it when a real server is ready to receive requests.
+HttpClientInMemoryWebApiModule.forRoot(
+  InMemoryDataService, { dataEncapsulation: false }
+)
   ],
   providers: [],
   bootstrap: [AppComponent]
